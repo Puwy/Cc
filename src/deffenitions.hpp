bc1172df347548fae6e7b2eeca603e306a1e78e6
Admin <faderotate@gmail.com>
2025-03-13 09:28:55 +0300

    1

--- src/deffenitions.hpp
+++ src/deffenitions.hpp
@@ -29,6 +29,15 @@ public:
         : position(new uint16_t(position)) {
     }
 
+    ~PolynomialMember() {
+        delete position;
+        delete[] constants;
+        delete members;
+    }
+
+    ConstantMember setVariable(int64_t base, int64_t exponent) {
+    }
+
 private:
     uint16_t* position;
     ConstantMember* constants;
@@ -37,14 +46,37 @@ private:
 
 struct ConstantMember {
 public:
+    ConstantMember(uint16_t position)
+        : position(new uint16_t(position)) {
+    }
     ConstantMember(string constant_expression, uint16_t position)
         : position(new uint16_t(position)) {
     }
 
+    ~ConstantMember() {
+        delete position;
+        delete base;
+        delete exponent;
+    }
+
+    ConstantMember operator+(const ConstantMember& other) {
+        ConstantMember result{*this->position};
+        if (*this->exponent > *other.exponent) {
+            result.base = new int64_t(*this->base);
+            *result.base *= pow(10, *this->exponent - *other.exponent);
+            *result.base += *other.base;
+            result.exponent = new int64_t(*other.exponent);
+            return result;
+        } else {
+            result.base = new int64_t(*other.base);
+            *result.base *= pow(10, *other.exponent - *this->exponent);
+        }
+    }
+
 private:
     uint16_t* position;
-    uint64_t* base;
-    uint64_t* exponent;
+    int64_t* base;
+    int64_t* exponent;
 };
 
 class Expression {
